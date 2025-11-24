325780ce8e1eac57dbac22a9644262a5a65e3ceb
Arka Mandal <152681694+AM5353@users.noreply.github.com>
2025-11-24 21:37:43 +0530

    Add image resizing script for specified folder
    This script resizes all images in the specified input folder to a new size of 300x300 pixels and saves them in an output folder.

--- Script that resizes all images in a folder.py
+++ Script that resizes all images in a folder.py
@@ -1,1 +1,35 @@
+from PIL import Image
+import os
+
+# Folder that contains original images
+input_folder = "images"
+
+# Folder where resized images will be saved
+output_folder = "resized_images"
+
+# Create output folder if it doesn't exist
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# Desired size (width, height)
+new_size = (300, 300)
+
+# Loop through all files in folder
+for filename in os.listdir(input_folder):
+    if filename.lower().endswith((".png", ".jpg", ".jpeg")):
+        img_path = os.path.join(input_folder, filename)
+        
+        # Open image
+        img = Image.open(img_path)
+
+        # Resize image
+        resized_img = img.resize(new_size)
+
+        # Save resized image to output folder
+        save_path = os.path.join(output_folder, filename)
+        resized_img.save(save_path)
+
+        print(f"Resized: {filename}")
+
+print("✔ All images resized successfully!")
 
